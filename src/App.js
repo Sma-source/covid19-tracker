7ec2f9a1cd774980fbfd4420d5696497a2a26eeb
Sma-source <rabhi.smain@gmail.com>
2021-12-11 20:33:28 +0100

    fetch data and pass it to Cards components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import { Chart, Cards, CountryPicker } from "./components";
 import styles from "./App.module.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
+
 function App() {
+  const [covidData, setCovidData] = useState([]);
+  const url = "https://covid19.mathdro.id/api";
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { data } = await axios.get(url);
+        const mData = {
+          confirmed: data.confirmed,
+          recovered: data.recovered,
+          deaths: data.deaths,
+        };
+        console.log(mData);
+        setCovidData(mData);
+      } catch (error) {}
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className={styles.container}>
       <h1>Hello World</h1>
-      <Cards />
+      <Cards covidData={covidData} />
       <Chart />
       <CountryPicker />
     </div>
