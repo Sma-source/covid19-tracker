5ad8947fb2683de25323c2cf2d8a97d453d5e966
Sma-source <rabhi.smain@gmail.com>
2021-12-11 22:13:01 +0100

    import cx and add media queries

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Card, CardContent, Typography, Grid } from "@material-ui/core";
 import CountUp from "react-countup";
-
+import cx from "classnames";
 import styles from "./Cards.module.css";
 const Cards = ({ covidData: { confirmed, recovered, deaths, lastUpdate } }) => {
   if (!confirmed) {
@@ -10,7 +10,13 @@ const Cards = ({ covidData: { confirmed, recovered, deaths, lastUpdate } }) => {
   return (
     <div className={styles.container}>
       <Grid container spacing={3} justify="center">
-        <Grid item component={Card}>
+        <Grid
+          item
+          component={Card}
+          xs={12}
+          md={3}
+          className={cx(styles.card, styles.infected)}
+        >
           <CardContent>
             <Typography color="textSecondary" gutterBottom>
               Infected
@@ -31,7 +37,13 @@ const Cards = ({ covidData: { confirmed, recovered, deaths, lastUpdate } }) => {
             </Typography>
           </CardContent>
         </Grid>
-        <Grid item component={Card}>
+        <Grid
+          item
+          component={Card}
+          xs={12}
+          md={3}
+          className={cx(styles.card, styles.recovered)}
+        >
           <CardContent>
             <Typography color="textSecondary" gutterBottom>
               Recovered
@@ -52,7 +64,13 @@ const Cards = ({ covidData: { confirmed, recovered, deaths, lastUpdate } }) => {
             </Typography>
           </CardContent>
         </Grid>
-        <Grid item component={Card}>
+        <Grid
+          item
+          component={Card}
+          xs={12}
+          md={3}
+          className={cx(styles.card, styles.death)}
+        >
           <CardContent>
             <Typography color="textSecondary" gutterBottom>
               Death
